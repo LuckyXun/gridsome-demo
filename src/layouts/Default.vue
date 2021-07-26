31ad2ab23c8fb877b40d383ce315682b4c889d9d
XunL <18380422673@163.com>
2021-07-26 23:20:10 +0800

    first commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="layout sticky-header">
+    <header class="header sticky">
+      <div class="container">
+        <div class="left">
+          <g-link
+            to="/"
+            class="home-link active"
+          >
+            <g-image
+              src="../assets/img/logo.svg"
+              class="logo"
+            />
+          </g-link>
+        </div>
+        <nav class="nav right">
+          <g-link
+            to="/journal"
+            class="nav__link"
+          >Journal</g-link>
+          <g-link
+            to="/contact"
+            class="nav__link"
+          >Say Hi!</g-link>
+        </nav>
+      </div>
+    </header>
+    <slot />
+    <footer class="footer">
+      <div class="container"><span>Copyright © 2021</span></div>
+    </footer>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+  
+}
+</script>
+
+
+<style lang="css">
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
+  font-size: 16px;
+  --color-base: #fff;
+  --color-base-1: #f3f3f3;
+  --color-contrast: #000;
+  --color-contrast-1: #2b2b2b;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, Segoe UI, Roboto,
+    Helvetica Neue, Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  font-size: 16px;
+  background: var(--color-base);
+  color: var(--color-contrast);
+  transition: background 0.5s ease;
+}
+
+.sticky-header {
+  padding: 6rem 0 0;
+}
+.header.sticky {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+.header {
+  position: relative;
+  height: 6rem;
+  z-index: 10;
+}
+.header > .container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 100%;
+}
+
+.nav > * {
+  font-size: 0.9rem;
+  font-weight: 600;
+  text-decoration: none;
+  margin-top: 4px;
+  margin-right: 3rem;
+  color: #000;
+  padding-bottom: 4px;
+  border-bottom: 1px solid;
+  border-color: transparent;
+  transition: border 0.15s;
+}
+
+.nav > *:hover {
+  border-bottom: 1px solid;
+}
+.footer {
+  font-size: 0.8rem;
+  padding: 6rem 0;
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 2rem;
+}
+@media (min-width: 860px) {
+  .container {
+    padding: 0 6rem;
+  }
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,113 @@
+<template>
+  <Layout>
+
+    <div>
+      <div class="latest-journals-heading container"><span class="label">Latest and greatest</span></div>
+      <div class="latest-journals">
+        <div class="container">
+          <g-link
+            to="/journal/gridsome-forestry-cms/"
+            class="journal"
+          >
+            <h3 class="journal-title">Gridsome with Forestry CMS</h3>
+          </g-link>
+          <g-link
+            to="/journal/use-gridsome-vuejs/"
+            class="journal"
+          >
+            <h3 class="journal-title">VueJS for your next project</h3>
+          </g-link>
+          <g-link
+            to="/journal/macos-development-environment/"
+            class="journal"
+          >
+            <h3 class="journal-title">macOS development environment</h3>
+          </g-link>
+          <g-link
+            to="/journal/a-journal-entry/"
+            class="journal"
+          >
+            <h3 class="journal-title">About Typography</h3>
+          </g-link>
+        </div>
+      </div>
+    </div>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style lang="css">
+.latest-journals-heading {
+  margin-top: 6rem;
+  margin-bottom: 1rem;
+  font-size: 0.6rem;
+  font-weight: 400;
+  text-transform: uppercase;
+}
+
+.latest-journals {
+  max-width: 100%;
+  margin: 0 2rem;
+  border: 1px solid var(--color-base-1);
+}
+.latest-journals > .container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+
+.journal {
+  flex: 0 0 100%;
+  display: block;
+  padding: 2rem;
+  transition: background 0.25s ease;
+  text-decoration: none;
+  border-bottom: 1px solid var(--color-base-1);
+}
+@media (min-width: 920px) {
+  .journal:first-child,
+  .journal:nth-child(2),
+  .journal:nth-child(3) {
+    border: 0;
+    border-right: 1px solid var(--color-base-1);
+  }
+}
+@media (min-width: 580px) {
+  .journal:first-child,
+  .journal:nth-child(2) {
+    border-bottom: 1px solid var(--color-base-1);
+  }
+}
+@media (min-width: 920px) {
+  .latest-journals {
+    margin: 0;
+    border-left: 0;
+    border-right: 0;
+    border-top: 1px solid var(--color-base-1);
+    border-bottom: 1px solid var(--color-base-1);
+  }
+}
+@media (min-width: 580px) {
+  .journal:first-child {
+    border-right: 1px solid var(--color-base-1);
+  }
+}
+@media (min-width: 920px) {
+  .journal {
+    flex: 0 0 25%;
+  }
+}
+@media (min-width: 580px) {
+  .journal {
+    flex: 0 0 50%;
+  }
+}
+</style>
